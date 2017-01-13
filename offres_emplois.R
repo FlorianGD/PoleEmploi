a828e213097a90a46737e85e8e6fa31a31c116d1
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2017-01-13 15:27:26 +0100

    Continue analysis

--- offres_emplois.R
+++ offres_emplois.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-options("scipen"=10) 
+library(lubridate)
+library(broom)
+options("scipen"=10, digits = 5) 
 
 offres <- read.csv2("offres.csv",stringsAsFactors = FALSE)
 
@@ -37,12 +39,13 @@ ggplot(data = offres_OEE, aes(Periode, nombre, color = categorie)) +
   theme(axis.text.x = element_text(angle = 90),
         legend.position = "bottom")
 
-library(lubridate)
-ggplot(data = offres_OEE %>% filter(year(Periode) >= 2007), aes(Periode, nombre, color = categorie)) +
+ggplot(data = offres_OEE %>% filter(year(Periode) >= 2005,
+                                    categorie %in% c("OEE_A", "OEE_B")), 
+       aes(Periode, nombre, color = categorie)) +
   geom_line() +
   geom_smooth(se = FALSE, method = "loess") +
   labs(title = "Nombre d'offres d'emplois par mois", 
-       subtitle = "De 1996 à 2015, par catégorie\nA est une offre d'emploi de plus de 6 mois, B entre 1 et 6 mois, et C inférieur à 1 mois",
+       subtitle = "De 2005 à 2015, par catégorie\nA est une offre d'emploi de plus de 6 mois, B entre 1 et 6 mois",
        caption = "Source : pôle emploi",
        x = "Année",
        y = "Nombre d'offres (milliers)",
@@ -54,3 +57,58 @@ ggplot(data = offres_OEE %>% filter(year(Periode) >= 2007), aes(Periode, nombre,
   scale_y_continuous(breaks = seq(0, 150, by = 25)) +
   theme(axis.text.x = element_text(angle = 0),
         legend.position = "bottom")
+
+ggplot(offres_long %>% filter(substr(categorie, 1, 2) == "To"),
+       aes(Periode, nombre)) +
+  geom_line(color = "tomato") +
+  geom_smooth(method = "loess") +
+  facet_wrap(~ categorie, scales = "free_y", ncol = 1) +
+  theme_minimal()
+
+# Plus grande variabilité dans les DOM (avec un volume plus faible), tendances
+# semblables
+# Vérifions à partir d'un modèle linéaire simple
+
+offres_totales <- offres %>% 
+  select(Periode, starts_with("Tot"))
+
+mod_lm_France <- lm(Total_France ~ Periode, offres_totales)
+mod_lm_France_df <- tidy(mod_lm_France)
+mod_lm_DOM <- lm(Total_DOM ~ Periode, offres_totales)
+mod_lm_DOM_df <- tidy(mod_lm_DOM)
+
+models <- bind_rows("France" = mod_lm_France_df,"DOM" = mod_lm_DOM_df, .id = "Perimetre")
+models
+# Sur la période, nous ne voyons pas la même tendance : positive et relativement
+# bonne (p.value ~5e-3) pour la France, mais légèrement négative pour les DOM
+
+offres_tot_groupe <- offres_long %>% 
+  filter(substr(categorie, 1, 3) == "Tot") %>%
+  mutate(groupe = factor(ifelse(Periode < as.Date("2008-01-01"),
+                                   "Avant 2008", "Après 2008"),
+                             levels = c("Avant 2008", "Après 2008")))
+           
+ggplot(offres_tot_groupe, aes(Periode, nombre, color = categorie)) +
+  geom_line() +
+  facet_grid(categorie ~ groupe, scales = "free") +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal()
+
+# Avant et après la crise de 2008, les tendances sont nettement différentes
+
+offres_nest <- offres_tot_groupe %>% 
+  nest(-groupe, - categorie) %>% 
+  mutate(models = map(data, ~ lm(nombre ~ Periode, data = .))) %>% 
+  mutate(tidied = map(models, tidy)) %>% 
+  unnest(tidied)
+
+offres_tot_groupe %>% 
+  nest(-groupe, - categorie) %>% 
+  mutate(models = map(data, ~ lm(nombre ~ Periode, data = .))) %>% 
+  `$`(models) %>% 
+  map(summary)
+
+offres_nest %>% 
+  group_by(term) %>%
+  mutate(p.adjusted = p.adjust(p.value)) %>% 
+  select(-statistic, -p.value)
